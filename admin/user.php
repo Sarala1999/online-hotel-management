1a1883d9092b0e5e061f744dbc47429b751ee011
Sarala1999 <saralagowda70900@gmail.com>
2020-08-17 01:22:00 +0530

    commited

---
+++ admin/user.php
@@ -0,0 +1,200 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+?> 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Administrator	</title>
+    <!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- Morris Chart Styles-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="user.php"> <?php echo $_SESSION["user"]; ?> </a>
+              
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+        </nav>
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="active-menu" href="user.php"><i class="fa fa-dashboard"></i> Status</a>
+                    </li>
+                    
+                   </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            Status <small>Room Booking </small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+				
+
+					<div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            
+                        </div>
+                        <div class="panel-body">
+                            <div class="panel-group" id="accordion">
+							
+							<div class="panel panel-primary">
+                                    
+                                    <div id="collapseTwo" class="panel-collapse in" style="height: auto;">
+                                        <div class="panel-body">
+                                           <div class="panel panel-default">
+                        
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Country</th>
+											<th>Room</th>
+											<th>Bedding</th>
+											<th>Meal</th>
+											<th>Check In</th>
+											<th>Check Out</th>
+											<th>Status</th>
+										 </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+									<?php
+                                    include ('db.php');
+                                  $email=$_SESSION["user"];
+					
+									$tsql = "select * from roombook where Email='$email'";
+									$tre = mysqli_query($con,$tsql);
+									while($trow=mysqli_fetch_array($tre) )
+									{	
+										
+										
+											echo"<tr>
+												<th>".$trow['id']."</th>
+												<th>".$trow['FName']." ".$trow['LName']."</th>
+												<th>".$trow['Email']."</th>
+												<th>".$trow['Country']."</th>
+												<th>".$trow['TRoom']."</th>
+												<th>".$trow['Bed']."</th>
+												<th>".$trow['Meal']."</th>
+												<th>".$trow['cin']."</th>
+												<th>".$trow['cout']."</th>
+												<th>".$trow['stat']."</th>
+												</tr>";
+										
+									
+									}
+									?>
+                                        
+                                    </tbody>
+                                </table>
+								
+                            </div>
+                        </div>
+                    </div>
+                      <!-- End  Basic Table  --> 
+                                        </div>
+                                    </div>
+                                </div>
+								
+                               
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+			
+				<!-- DEOMO-->
+				
+                        </div>
+				
+				<!--DEMO END-->
+				
+										
+                    
+
+                <!-- /. ROW  -->
+				
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- Morris Chart Js -->
+    <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+    <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+
+
+</body>
+
+</html>
